f5ba12e4ce4ec217c12b58aed3dd547106ba26be
alrafi <hafisalrafi@gmail.com>
2020-03-14 16:34:56 +0700

    update countryCode based on selected country

--- src/components/CuntrySelector.js
+++ src/components/CuntrySelector.js
@@ -11,21 +11,24 @@ const CountrySelector = () => {
 
   return (
     <div>
-      <div>
-        <ReactCountryFlag
-          countryCode="ID"
-          svg
-          cdnUrl="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.4.3/flags/1x1/"
-          cdnSuffix="svg"
-          title="US"
-        />
-        <p>{selectedCountry}</p>
+      <div className="country">
+        <div className="country__info">
+          <ReactCountryFlag
+            countryCode={selectedCountry}
+            svg
+            cdnUrl="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.4.3/flags/1x1/"
+            cdnSuffix="svg"
+            title="US"
+          />
+          <p>{selectedCountry}</p>
+        </div>
         <select
           onChange={e => {
             console.log(e.target.value);
             setSelectedCountry(e.target.value);
           }}
           value={selectedCountry}
+          className="country__select"
         >
           {Object.entries(listCountries.countries).map(([country, code]) => (
             <option key={code} value={code}>
