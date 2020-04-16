47f7e7deed5d16f2628e440077d5602708a34985
alrafi <hafisalrafi@gmail.com>
2020-04-16 21:01:18 +0700

    add useStat to pass selected country

--- src/components/StatsContainer.js
+++ src/components/StatsContainer.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import Stats from './Stats';
 import CountrySelector from './CuntrySelector';
+import Footer from './Footer';
 import styled from 'styled-components';
+import useStats from '../utils/useStats';
 
 const StatsWrapper = styled.div`
   padding: 20px 15px;
@@ -25,6 +27,9 @@ const StatsUpdate = styled.div`
 
 const StatsContainer = () => {
   const [selectedCountry, setSelectedCountry] = useState('ID');
+  const stats = useStats();
+
+  if (!stats) return <p>Loading...</p>;
 
   const onCountrySelect = (selectedCountry) => {
     console.log(selectedCountry);
@@ -36,9 +41,10 @@ const StatsContainer = () => {
       <CountrySelector onSelect={onCountrySelect}></CountrySelector>
       <StatsUpdate>
         <h3>COVID-19 Cases Update</h3>
-        <p>Last Update: 2020-03-26</p>
+        <p>Last Update: {stats.lastUpdate.substring(0, 10)}</p>
         <Stats url={`countries/${selectedCountry}`}></Stats>
       </StatsUpdate>
+      <Footer></Footer>
     </StatsWrapper>
   );
 };
