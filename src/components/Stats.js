f5a6c9cd1d15b2934c734ac681e3ad26a0503345
alrafi <hafisalrafi@gmail.com>
2020-04-16 21:01:52 +0700

    refactor stats and update styled components

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,5 +1,42 @@
 import React from 'react';
 import useStats from '../utils/useStats';
+import styled from 'styled-components';
+import confirmedIcon from '../assets/confirmed_icon.png';
+import recoveredIcon from '../assets/recovered_icon.png';
+import deathIcon from '../assets/death_icon.png';
+
+const StatsWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background: #ffffff;
+  box-shadow: 0px 15px 30px rgba(33, 140, 173, 0.05);
+  border-radius: 20px;
+  padding: 10px 15px;
+  margin-top: 20px;
+`;
+
+const BoxStats = styled.div`
+  width: 30%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+
+  p {
+    font-size: 80% !important;
+    color: #99a6c0;
+  }
+
+  h3 {
+    color: ${(props) => props.color};
+    font-size: 120% !important;
+    margin: 3px 0;
+  }
+
+  img {
+    width: 40px;
+  }
+`;
 
 const Stats = ({ url }) => {
   const stats = useStats(url);
@@ -7,20 +44,23 @@ const Stats = ({ url }) => {
   if (!stats) return <p>Loading...</p>;
 
   return (
-    <div className="stats__container">
-      <div className="box confirmed">
-        <p>Confirmed</p>
+    <StatsWrapper>
+      <BoxStats color="#FF9E6B">
+        <img src={confirmedIcon} alt="confirmed" />
         <h3>{stats !== 'N/A' ? stats.confirmed.value : 'N/A'}</h3>
-      </div>
-      <div className="box recovered">
-        <p>Recovered</p>
+        <p>Confirmed</p>
+      </BoxStats>
+      <BoxStats color="#45D07C">
+        <img src={recoveredIcon} alt="recovered" />
         <h3>{stats !== 'N/A' ? stats.recovered.value : 'N/A'}</h3>
-      </div>
-      <div className="box deaths">
-        <p>Deaths</p>
+        <p>Recovered</p>
+      </BoxStats>
+      <BoxStats color="#F34B4B">
+        <img src={deathIcon} alt="death" />
         <h3>{stats !== 'N/A' ? stats.deaths.value : 'N/A'}</h3>
-      </div>
-    </div>
+        <p>Deaths</p>
+      </BoxStats>
+    </StatsWrapper>
   );
 };
 
