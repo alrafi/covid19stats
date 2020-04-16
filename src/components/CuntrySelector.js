314186a14f3f3e5512cef854180e3a9029a085d2
alrafi <hafisalrafi@gmail.com>
2020-04-16 11:35:44 +0700

    update country selector with syled components

--- src/components/CuntrySelector.js
+++ src/components/CuntrySelector.js
@@ -2,8 +2,43 @@ import React, { useState } from 'react';
 import useStats from '../utils/useStats';
 import Stats from './Stats';
 import ReactCountryFlag from 'react-country-flag';
+import styled from 'styled-components';
 
-const CountrySelector = () => {
+const CountryShowing = styled.div`
+  border: 1px solid #99a6c0;
+  border-radius: 100px;
+  padding: 0 10px;
+  /* margin: 0 20px; */
+`;
+
+const CountryWrapper = styled.div`
+  display: flex;
+  margin: 10px;
+`;
+
+const CountryInfo = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: 10px;
+
+  img {
+    border-radius: 50%;
+    margin-right: 5px;
+  }
+`;
+
+const CountrySelect = styled.select`
+  width: 100%;
+  padding: 5px;
+  border: none;
+  font-size: 80%;
+  /* border-radius: 10px; */
+  background-color: transparent;
+  outline: none;
+`;
+
+const CountrySelector = (props) => {
   const listCountries = useStats('countries');
   const [selectedCountry, setSelectedCountry] = useState('ID');
 
@@ -12,9 +47,9 @@ const CountrySelector = () => {
 
   return (
     // console.loh(listCountries);
-    <div className="country__showing">
-      <div className="country">
-        <div className="country__info">
+    <CountryShowing>
+      <CountryWrapper>
+        <CountryInfo>
           <ReactCountryFlag
             countryCode={selectedCountry}
             svg
@@ -23,10 +58,11 @@ const CountrySelector = () => {
             title="US"
           />
           <p>{selectedCountry}</p>
-        </div>
-        <select
+        </CountryInfo>
+        <CountrySelect
           onChange={(e) => {
             setSelectedCountry(e.target.value);
+            props.onSelect(e.target.value);
           }}
           value={selectedCountry}
           className="country__select"
@@ -36,10 +72,10 @@ const CountrySelector = () => {
               {country.name}
             </option>
           ))}
-        </select>
-      </div>
-      <Stats url={`countries/${selectedCountry}`} />
-    </div>
+        </CountrySelect>
+      </CountryWrapper>
+      {/* <Stats url={`countries/${selectedCountry}`} /> */}
+    </CountryShowing>
   );
 };
 
